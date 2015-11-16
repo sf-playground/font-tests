39d9e2e8b6ef2ba49f9b445c1fc106fac3d1e3a6
Chris Simpkins <git.simpkins@gmail.com>
2015-11-15 20:20:01 -0500

    added MetricsObject

--- test-metrics.py
+++ test-metrics.py
@@ -16,7 +16,32 @@ except ImportError:
 
 class MetricsObject(object):
     def __init__(self, fontpath):
-        pass
+        unitsPerEm = 0              # [head] unitsPerEm
+        ascent = 0                  # [hhea] ascent
+        descent = 0                 # [hhea] descent
+        lineGap = 0                 # [hhea] lineGap
+        capheight = 0               # [OS/2] sCapHeight
+        xheight = 0                 # [OS/2] sxHeight
+        typoAscender = 0            # [OS/2] sTypoAscender
+        typoDescender = 0           # [OS/2] sTypoDescender
+        typoLineGap = 0             # [OS/2] sTypoLineGap
+        winAscent = 0               # [OS/2] usWinDescent
+        winDescent = 0              # [OS/2] usWinAscent
+        strikeoutPosition = 0       # [OS/2] yStrikeoutPosition
+        strikeoutSize = 0           # [OS/2] yStrikeoutSize
+        averageWidth = 0            # [OS/2] xAvgCharWidth
+        superscriptXSize = 0        # [OS/2] ySuperscriptXSize
+        superscriptXOffset = 0      # [OS/2] ySubscriptXOffset
+        superscriptYSize = 0        # [OS/2] ySuperscriptYSize
+        superscriptYOffset = 0      # [OS/2] ySuperscriptYOffset
+        subscriptXSize = 0          # [OS/2] ySubscriptXSize
+        subscriptXOffset = 0        # [OS/2] ySubscriptXOffset
+        subscriptYSize = 0          # [OS/2] ySubscriptYSize
+        subscriptYOffset = 0        # [OS/2] ySubscriptYOffset
+        underlinePosition = 0       # [post] underlinePosition
+        underlineThickness = 0      # [post] underlineThickness
+        italicAngle = 0             # [post] italicAngle
+
 
 def main(maps):
 
@@ -32,6 +57,7 @@ def main(maps):
                             expected_stream = open(expectedpath, "r")
                             expected_dict = load(expected_stream, Loader=Loader)
                             print(expected_dict)
+                            sys.exit(0)
 
                             # TODO : parse YAML file for expected data
                         except Exception as e:
