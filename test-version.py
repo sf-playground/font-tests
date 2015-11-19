34872afd67907d2eda20a6273ca1400439956fee
Chris Simpkins <git.simpkins@gmail.com>
2015-11-18 23:01:22 -0500

    added version string parsing code from name table ID 5

--- test-version.py
+++ test-version.py
@@ -13,7 +13,37 @@ import os.path
 from fontTools import ttLib
 
 def main(arguments):
-    pass
+    # Begin report
+    print("\nBegin test-version.py font version tests...\n")
+
+    ERROR_OCCURRED = False
+    expected_version = arguments[-1]
+
+    filepaths = arguments[0:-1]
+
+    for fontpath in filepaths:
+        if os.path.isfile(fontpath):
+            print("\n>>> Testing '" + fontpath + "'\n")
+            tt = ttLib.TTFont(fontpath)
+
+            version_string_raw = ""
+            name_tables = tt['name'].__dict__['names']
+            for name_table in name_tables:
+                if name_table.__dict__['nameID'] == 5:
+                    version_string_raw = name_table.__dict__['string']
+
+            if len(version_string_raw) > 0:
+                pass
+                # TODO: parse the version number from the raw version string and compare
+            else:
+                sys.stderr.write("[test-version.py] ERROR: unable to parse the version string from the nameID = 5 table for '" + fontpath + "'.")
+                ERROR_OCCURRED = True
+
+    if ERROR_OCCURRED is True:
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
 
 
 if __name__ == '__main__':
