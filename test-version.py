9453f4c19edb79a48894707f3a30844d659b4d01
Chris Simpkins <git.simpkins@gmail.com>
2015-11-23 19:58:05 -0500

    added support for platformID = 1 and platformID = 3 version name tables

--- test-version.py
+++ test-version.py
@@ -34,25 +34,47 @@ def main(arguments):
             version_string_raw = ""
             name_tables = tt['name'].__dict__['names']
             for name_table in name_tables:
-                if name_table.__dict__['nameID'] == 5:
-                    version_string_raw = name_table.__dict__['string']
+                if name_table.__dict__['nameID'] == 5 and name_table.__dict__['platformID'] == 1:
+                    osx_version_string_raw = name_table.__dict__['string']
+                elif name_table.__dict__['nameID'] == 5 and name_table.__dict__['platformID'] == 3:
+                    win_version_string_raw = name_table.__dict__['string']
 
-            if len(version_string_raw) > 0:
-                # UTF-16 big endian encoded table string, need to convert
-                version_string_decoded = bytes.decode(version_string_raw, "utf-16-be")
-                if version_string_decoded.startswith("Version"):
-                    m = pattern.search(version_string_decoded)
-                    observed_version = m.group('version')
-                    if observed_version == expected_version:
-                        print("[test-version.py] Expected version string '" + expected_version + "' was detected in the name tables.\n")
+            # nameID = 5, platformID = 1 (OS X)
+            if len(osx_version_string_raw) > 0:
+                if type(osx_version_string_raw) == bytes:
+                    osx_version_string_raw = bytes.decode(osx_version_string_raw, "utf-8")
+                if osx_version_string_raw.startswith("Version"):
+                    m = pattern.search(osx_version_string_raw)
+                    osx_observed_version = m.group('version')
+                    if osx_observed_version == expected_version:
+                        print("[test-version.py] Expected version string '" + expected_version + "' was detected in the name tables for nameID=5, platformID=1.\n\n")
                     else:
-                        sys.stderr.write("[test-version.py] ERROR: Expected version '" + expected_version + "' does not equal observed version '" + observed_version + "'\n")
+                        sys.stderr.write("[test-version.py] ERROR: Expected version '" + expected_version + "' does not equal observed version '" + osx_observed_version + "' for nameID=5, platformID=1\n\n")
                         ERROR_OCCURRED = True
                 else:
-                    sys.stderr.write("[test-version.py] ERROR: Did not detect 'Version X.XXX' syntax at the beginning of the version string in nameID=5 name table.")
+                    sys.stderr.write("[test-version.py] ERROR: Did not detect 'Version X.XXX' syntax at the beginning of the version string in nameID=5, platformID=1 name table.\n\n")
                     ERROR_OCCURRED = True
             else:
-                sys.stderr.write("[test-version.py] ERROR: unable to parse the version string from the nameID = 5 table for '" + fontpath + "'.")
+                sys.stderr.write("[test-version.py] ERROR: unable to parse the version string from the nameID = 5, platformID=1 table for '" + fontpath + "'.\n\n")
+                ERROR_OCCURRED = True
+
+            # nameID = 5, platformID = 3 (Windows)
+            if len(win_version_string_raw) > 0:
+                # UTF-16 big endian encoded table string in Windows name tables, need to convert
+                win_version_string_decoded = bytes.decode(win_version_string_raw, "utf-16-be")
+                if win_version_string_decoded.startswith("Version"):
+                    m = pattern.search(win_version_string_decoded)
+                    win_observed_version = m.group('version')
+                    if win_observed_version == expected_version:
+                        print("[test-version.py] Expected version string '" + expected_version + "' was detected in the name tables for nameID=5, platformID=3.\n\n")
+                    else:
+                        sys.stderr.write("[test-version.py] ERROR: Expected version '" + expected_version + "' does not equal observed version '" + win_observed_version + "' for nameID=5, platformID=3\n\n")
+                        ERROR_OCCURRED = True
+                else:
+                    sys.stderr.write("[test-version.py] ERROR: Did not detect 'Version X.XXX' syntax at the beginning of the version string in nameID=5, platformID=3 name table.\n\n")
+                    ERROR_OCCURRED = True
+            else:
+                sys.stderr.write("[test-version.py] ERROR: unable to parse the version string from the nameID = 5, platformID=3 table for '" + fontpath + "'.\n\n")
                 ERROR_OCCURRED = True
 
     if ERROR_OCCURRED is True:
